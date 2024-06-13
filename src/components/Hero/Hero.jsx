c9ec31e1e3ecbc7ee8bc8b0f602598ed51ed0449
Aman Rajput <rajput.aman922@gmail.com>
2024-06-13 12:56:38 -0700

    s-commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,9 @@ import  './HeroStyles.css';
 import heroImg from '../../assets/hero-img.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
+import behance from '../../assets/behance.svg';
+import behanceD from '../../assets/behancedark.svg';
+// import twitterDark from '../../assets/twitter-dark.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
@@ -16,7 +17,7 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const behanceIcon = theme === 'light' ? behance : behanceD;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
@@ -41,13 +42,13 @@ function Hero() {
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
+          <a href="https://www.behance.net/appysinghx8720" target="_blank">
+            <img src={behanceIcon} alt="Twitter icon" />
           </a>
           <a href="https://github.com/Amansinghraj" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/aman-rajput99/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
@@ -56,7 +57,7 @@ function Hero() {
           businesses.
         </p>
         <a href={CV} download>
-          <button className="hover">Resume</button>
+          <button className="hover">Resume!</button>
         </a>
       </div>
     </section>
@@ -64,3 +65,6 @@ function Hero() {
 }
 
 export default Hero;
+
+
+
